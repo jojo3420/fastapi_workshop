3a6bd6d4640c03b8fa798d6443a99fbe8b101417
jojo3420 <jjjhhhvvv@gmail.com>
2021-12-14 10:47:03 +0900

    File 처리 연습과 await async 사용

---
+++ app/main2.py
@@ -0,0 +1,57 @@
+import time
+from typing import IO
+from tempfile import NamedTemporaryFile
+
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post('/file/size')
+def get_file(file: bytes = File(...)):
+    # bytes 타입을 바이트 스트럼 으로 받기
+    print(type(file))
+    return {
+        'file_size': len(file)
+    }
+
+
+@app.post('/file/info')
+async def echo_file_info(file: UploadFile = File(...)):
+    # await file.write()
+    file_like_obj = file.file
+    content = await file.read()
+    # print(content)
+    return {
+        'filename': file.filename,
+        'content_type': file.content_type,
+    }
+
+
+async def store_file(file: IO):
+    """  store file to local storage """
+    # time.sleep(3)
+    with NamedTemporaryFile('wb', delete=False) as temp_file:
+        temp_file.write(file.read())
+        return temp_file.name
+
+
+@app.post('/file/save')
+async def save_file(file: UploadFile = File(...)):
+    for i in range(5):
+        print(i)
+    # print('-' * 100)
+    path = await store_file(file.file)
+    # print(path)
+    for j in range(6, 10):
+        print(j)
+    # print('-' * 100)
+
+    return {
+        'file_path': path
+    }
+
+
+if __name__ == '__main__':
+    uvicorn.run('main2:app', reload=True)
