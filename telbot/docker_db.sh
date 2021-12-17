7cd45f7e8d4a64e7738530573927ae13ca276a58
jgura3420 <jjjhhhvvv@gmail.com>
2021-12-17 20:10:42 +0900

    docker_db.sh 변경

--- telbot/docker_db.sh
+++ telbot/docker_db.sh
@@ -1,9 +1,9 @@
 # create a container
 $ docker run --name fastapi-db \
 -p 3306:3306 \
--e MYSQL_ROOT_PASSWORD=1234 \
--e MYSQL_USER=admin \
--e MYSQL_PASSWORD=1234 \
+-e MYSQL_ROOT_PASSWORD=dkagh12#$ \
+-e MYSQL_USER=root \
+-e MYSQL_PASSWORD=dkagh12#$ \
 -d mysql:8.0 \
 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
