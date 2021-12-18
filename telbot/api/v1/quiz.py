41123be1a85ca446462137ff39a3d3555c8e1b86
jojo3420 <jjjhhhvvv@gmail.com>
2021-12-18 15:30:50 +0900

    - form 전용 라우터 추가(Form) - 로그인, 퀴즈문제 입력 화면 구현 - 텔레그램 로그인 추가 /static/login.html - 텥레그램 봇 설정

--- telbot/api/v1/quiz.py
+++ telbot/api/v1/quiz.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, status
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
@@ -11,19 +11,39 @@ from telbot import models
 router = APIRouter()
 
 
-@router.get("", response_model=List[Quiz])
-def get_quiz_list(conn: Session = Depends(get_conn)):
-    return conn.query(models.Quiz).all()
-
-
-@router.post("", response_model=ResourceId)
-def register_quiz(data: QuizCreate, conn: Session = Depends(get_conn)):
-    quiz = models.Quiz(**data.dict())
+async def add_quiz(
+    conn: Session, question: str, examples: str, answer: int
+) -> models.Quiz:
+    quiz = models.Quiz(question=question, examples=examples, answer=answer)
     conn.add(quiz)
     conn.commit()
     return quiz
 
 
+# /v1/quiz
+@router.get("", response_model=List[Quiz])
+def get_quiz_list(conn: Session = Depends(get_conn)):
+    return conn.query(models.Quiz).all()
+
+
+# /v1/quiz
+@router.post("", response_model=ResourceId, status_code=status.HTTP_201_CREATED)
+async def register_quiz(data: QuizCreate, conn: Session = Depends(get_conn)):
+    return await add_quiz(conn=conn, **data.dict())
+
+
+# /v1/quiz/form
+@router.post("/form", response_model=ResourceId, status_code=status.HTTP_201_CREATED)
+async def register_quiz_form(
+    question: str = Form(..., title="퀴즈질문"),
+    examples: str = Form(..., title="퀴즈 보기"),
+    answer: int = Form(..., title="정답"),
+    conn: Session = Depends(get_conn),
+):
+    return await add_quiz(conn, question, examples, answer)
+
+
+# /v1/quiz/pic-one
 @router.get("/pic-one", response_model=Quiz)
 def pic_one_by_random(conn: Session = Depends(get_conn)):
     return conn.query(models.Quiz).order_by(func.RAND()).first()
--- telbot/main.py
+++ telbot/main.py
@@ -1,20 +1,32 @@
 import uvicorn
 from fastapi import FastAPI, status
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from devtools import debug
 
 from telbot import models
-
 from telbot import api
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1",
+        "http://localhost",
+        " https://0101-49-166-116-160.ap.ngrok.io",
+    ],
+    allow_credentials=True,
+    allow_methods=["OPTION", "GET", "POST", "DELETE"],
+    allow_headers={"*"},
+)
 
 
 @app.on_event("startup")
 def startup():
     from telbot.database import engine
 
-    app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.mount("/static", StaticFiles(directory="telbot/static"), name="static")
     models.Base.metadata.create_all(bind=engine)
 
 
@@ -24,7 +36,6 @@ def health_check():
 
 
 app.include_router(api.router)
-
 
 if __name__ == "__main__":
     uvicorn.run("telbot.main:app", reload=True)
